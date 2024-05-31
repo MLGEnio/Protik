71b827adfc2e61800a8721952d7975c549728706
MlgEnio <eniosefa2@gmail.com>
2024-05-31 22:13:01 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-xl text-gray-900">
+          TEst
+      </h1>
+    </>
+  )
+}
+
+export default App

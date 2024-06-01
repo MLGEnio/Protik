4a21e2f4067dd3d9a58d399ff466b9f6c9601103
MlgEnio <eniosefa2@gmail.com>
2024-06-01 19:26:03 +0200

    finalized card design for swiper

--- src/pages/ExploreProjects.jsx
+++ src/pages/ExploreProjects.jsx
@@ -46,6 +46,7 @@ const projects = [
 const ExploreProjects = () => {
 	const [currentIndex, setCurrentIndex] = useState(projects.length - 1);
 	const [lastDirection, setLastDirection] = useState();
+	const [expandedCardIndex, setExpandedCardIndex] = useState(null);
 	const currentIndexRef = useRef(currentIndex);
 
 	const childRefs = useMemo(
@@ -78,6 +79,7 @@ const ExploreProjects = () => {
 
 	const swipe = async (dir) => {
 		if (canSwipe && currentIndex < projects.length) {
+			setExpandedCardIndex(null)
 			await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
 		}
 	};
@@ -94,7 +96,7 @@ const ExploreProjects = () => {
 
 	return (
 		<div className='min-h-screen text-white flex flex-col items-center p-4 overflow-hidden'>
-			<div className='w-full max-w-2xl relative'>
+			<div className='w-full max-w-full relative'>
 				{projects.map((project, index) => (
 					<TinderCard
 						ref={childRefs[index]}
@@ -105,23 +107,60 @@ const ExploreProjects = () => {
 						preventSwipe={["up", "down"]}
 					>
 						<div
+							onClick={() => {
+								if(expandedCardIndex) {
+									setExpandedCardIndex(null)
+								}else {
+									setExpandedCardIndex(index)
+								}
+							}}
 							style={{
-								backgroundImage: `url(${project.photo})`,
+								backgroundImage: `url(${expandedCardIndex === index ? "" :project.photo})`,
+								backgroundColor: 'rgba(174,175,255,0.1)' ,
+								backdropFilter: 'blur(10px)',
 								...getCardStyle(index),
 								zIndex: projects.length - index,
+								transition: 'transform 0.5s ease, width 0.5s ease, height 0.5s ease',
+								width: expandedCardIndex === index ? '1000px' : '400px',
+								height: expandedCardIndex === index ? '600px' : '500px',
 							}}
-							className='bg-cover bg-center rounded-lg shadow-lg p-6 h-[550px] w-[400px] flex flex-col justify-end'
+							className='rounded-lg bg-cover bg-center object-center fit-content shadow-lg p-6 flex flex-col justify-end'
 						>
-							<h3 className='text-2xl font-bold'>
-								{project.name}
-							</h3>
-
+							{expandedCardIndex !== index && (
+								<h3 className='text-2xl font-bold bg-black bg-opacity-20 rounded'>
+									{project.name}
+								</h3>
+							)}
+							{expandedCardIndex === index ? (
+								<div className="grid grid-cols-2 h-full">
+									<div className="flex flex-col p-6">
+										<h3 className='text-2xl font-bold'>
+											{project.name}
+										</h3>
+										<p className='mt-2 text-gray-300'>
+											{project.details}
+										</p>
+										<div className='mt-4'>
+											<h3 className='font-semibold'>Members</h3>
+											<ul className='list-disc list-inside'>
+												{project.members.map((member, index) => (
+													<li key={index}>{member}</li>
+												))}
+											</ul>
+										</div>
+									</div>
+									<img src={project.photo} alt={project.name} className="h-full max-h-[550px] bg-cover bg-center object-center fit-content ml-2 rounded-lg"/>
+								</div>
+							) : (
+								<p className='mt-2 text-gray-300 bg-black bg-opacity-20 rounded'>
+									Click to see more...
+								</p>
+							)}
 						</div>
 					</TinderCard>
 				))}
 			</div>
 			<div className='flex space-x-10 relative top-[300px] mt-4 z-50'>
-
 				<button
 					className='p-2 rounded-full flex justify-center items-center'
 					style={{border: "2px solid red"}}
